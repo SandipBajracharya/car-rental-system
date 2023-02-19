fe5e1df48f6ddab20977f31811f219be59f55327
SandipBajracharya <sndp106.sb@gmail.com>
2023-02-19 09:49:06 +0545

    default role value

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,7 +23,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->smallInteger('status')->default(1);
-            $table->unsignedBigInteger('role_id');
+            $table->unsignedBigInteger('role_id')->default(2);
             $table->string('mobile_number', 50)->nullable();
             $table->string('image')->nullable();
             $table->rememberToken();

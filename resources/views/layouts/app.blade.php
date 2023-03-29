270b270a423aaf273fce9abb9581f70f2cac7e48
SandipBajracharya <sndp106.sb@gmail.com>
2023-03-30 00:26:49 +0545

    title icon added

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="icon" href="{{asset('images/logo.svg') }}">
 </head>
 <body>
     <div id="app">
